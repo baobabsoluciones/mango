3c2dfe795ce436e7208082f4c601ceab079e205c
Guillermo González-Santander de la Cruz <guillermo.gonzalez@baobabsoluciones.es>
2022-10-17 17:11:19 +0200

    Feature/file tests (#7)
    * Added tests for file functions
    
    * Added test that makes the function broke
    
    * Added assert
    
    * - corrijo test_bad_folder
    
    * - cambio el tipo de error
    
    Co-authored-by: Lucas Camarena <lucas.camarena@baobabsoluciones.es>

---
+++ tests/test_file_functions.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from processing.file_functions import list_files_directory
+
+
+class FileTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_all_files(self):
+        some_files = [
+            "./processing\\date_functions.py",
+            "./processing\\file_functions.py",
+        ]
+        files = list_files_directory("./processing")
+        for file in some_files:
+            self.assertIn(file, files)
+
+    def test_get_some_files(self):
+        some_files = [".\\requirements-dev.txt", ".\\requirements.txt"]
+        files = list_files_directory(".", ["txt"])
+        for file in some_files:
+            self.assertIn(file, files)
+
+    def test_bad_folder(self):
+        self.assertRaises(FileNotFoundError, list_files_directory, "./no-exists")
