c1793e1a526955f0ac3bcd98c42b7a07d2096e7c
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-04-19 10:43:08 +0200

    Changes to logging

--- mango/logging/decorators.py
+++ mango/logging/decorators.py
@@ -2,38 +2,40 @@ import time
 from functools import wraps
 import logging
 
-logger = logging.getLogger("root")
+def log_time(logger: str = "root"):
+    logger = logging.getLogger(logger)
 
-
-def log_time(func):
-    """
-    The log_time function is a decorator that wraps the function func.
-    It takes the arguments and keyword arguments passed to it, passes them on to func,
-    and returns what func returns. However, before doing so it logs a message with log.info()
-    describing what function is being called and how long it took to execute.
-
-    :param func: pass the function to be decorated
-    :return: the result of the call to func
-    :doc-author: baobab soluciones
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
+    def log_decorator(func):
         """
-        The wrapper function is a decorator that wraps the function func.
+        The log_decorator function is a decorator that wraps the function func.
         It takes the arguments and keyword arguments passed to it, passes them on to func,
         and returns what func returns. However, before doing so it logs a message with log.info()
         describing what function is being called and how long it took to execute.
 
-        :param *args: pass a non-keyworded, variable-length argument list
-        :param **kwargs: catch all keyword arguments that are passed to the function
-        :return: The result of the function it wraps
+        :param func: pass the function to be decorated
+        :return: the result of the call to func
         :doc-author: baobab soluciones
         """
-        start = time.time()
-        result = func(*args, **kwargs)
-        duration = round(time.time() - start, 2)
-        logger.info(f"{func.__name__} took {duration} seconds")
-        return result
 
-    return wrapper
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """
+            The wrapper function is a decorator that wraps the function func.
+            It takes the arguments and keyword arguments passed to it, passes them on to func,
+            and returns what func returns. However, before doing so it logs a message with log.info()
+            describing what function is being called and how long it took to execute.
+
+            :param *args: pass a non-keyworded, variable-length argument list
+            :param **kwargs: catch all keyword arguments that are passed to the function
+            :return: The result of the function it wraps
+            :doc-author: baobab soluciones
+            """
+            start = time.time()
+            result = func(*args, **kwargs)
+            duration = round(time.time() - start, 2)
+            logger.info(f"{func.__name__} took {duration} seconds")
+            return result
+
+        return wrapper
+
+    return log_decorator
