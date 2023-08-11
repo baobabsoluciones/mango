f366609e1824c3049566fed1294af3180ebedb35
Hugo Larzabal <hugo.larzabal@baobabsoluciones.es>
2023-08-11 16:24:16 +0200

    try to refactor read and write excel without pandas

--- mango/processing/file_functions.py
+++ mango/processing/file_functions.py
@@ -3,6 +3,8 @@ from os import listdir
 from typing import Union
 
 import pandas as pd
+import openpyxl as xl
+from pytups import TupList
 
 
 def list_files_directory(directory: str, extensions: list = None):
@@ -228,3 +230,67 @@ def adjust_excel_col_width(writer, df: pd.DataFrame, table_name: str, min_len: i
         column_width = max(content_len, len(str(column)), min_len) + 2
         col_idx = df.columns.get_loc(column)
         writer.sheets[table_name].set_column(col_idx, col_idx, column_width)
+
+
+def load_excel_2(path):
+    """
+    The load_excel function loads an Excel file and returns it as a dictionary of DataFrames.
+    It doesn't use pandas.
+
+    :param path: Specify the path of the file to be loaded
+    :return: A dictionary of TupLists
+    :doc-author: baobab soluciones
+    """
+    if not is_excel_file(path):
+        raise FileNotFoundError(
+            f"File {path} is not an Excel file (.xlsx, .xls, .xlsm)."
+        )
+
+    wb = xl.load_workbook(path)
+    dataset = {}
+    for ws in wb:
+        data = [row for row in ws.values]
+        if len(data):
+            dataset[ws.title] = TupList(data[1:]).to_dictlist(data[0])
+        else:
+            dataset[ws.title] = []
+
+    return dataset
+
+
+def write_excel_2(path, data):
+    """
+    The write_excel function writes a dictionary of DataFrames to an Excel file.
+
+    :param path: Specify the path of the file that you want to write to
+    :param data: Specify the dictionary to be written
+    :return: None
+    :doc-author: baobab soluciones
+    """
+    if not is_excel_file(path):
+        raise FileNotFoundError(
+            f"File {path} is not an Excel file (.xlsx, .xls, .xlsm)."
+        )
+    wb = xl.Workbook()
+    if len(data):
+        wb.remove(wb.active)
+    from openpyxl.worksheet.table import Table, TableStyleInfo
+    for sheet_name, content in data.items():
+        wb.create_sheet(sheet_name)
+        if isinstance(content, list):
+            ws = wb[sheet_name]
+            if len(content):
+                ws.append([k for k in content[0].keys()])
+                for row in content:
+                    ws.append([v for v in row.values()])
+                #  TODO: look at interesting styles
+                style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                                       showLastColumn=False, showRowStripes=False, showColumnStripes=False)
+                columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                if len(content[0])<27:
+                    tab = Table(displayName="Table1", ref=f"A1:{columns[len(content[0])-1]}{len(content)+1}")
+                    # tab.tableStyleInfo = style
+                    ws.add_table(tab)
+
+    wb.save(path)
+
