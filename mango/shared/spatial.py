1bf718bb9d744cb04502a7b7493685a11db799d1
AntonioGonzalezSuarez <112699773+AntonioGonzalezSuarez@users.noreply.github.com>
2023-11-16 17:17:34 +0000

    Tests and validations

--- mango/shared/spatial.py
+++ mango/shared/spatial.py
@@ -1,16 +1,24 @@
 from math import radians, cos, sin, asin, sqrt
+from .decorators import pydantic_validation
+from pydantic import BaseModel, confloat
+from typing import Tuple
 
+class HaversineArgs(BaseModel):
+    point1: Tuple[confloat(ge=-90.0, le=90.0),confloat(ge=-180.0, le=180.0)]
+    point2: Tuple[confloat(ge=-90.0, le=90.0),confloat(ge=-180.0, le=180.0)]
 
+@pydantic_validation(HaversineArgs)
 def haversine(point1, point2) -> float:
     """
     Calculate the great circle distance in kilometers between two points
     on the earth (specified in decimal degrees).
+    If you need more precision use geopy implementation as it uses geodesics.
     :param point1: Tuple with latitude and longitude of the first point
     :param point2: Tuple with latitude and longitude of the second point
     """
     # convert decimal degrees to radians
-    lon1, lat1 = point1
-    lon2, lat2 = point2
+    lat1, lon1 = point1
+    lat2, lon2 = point2
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
     # haversine formula
