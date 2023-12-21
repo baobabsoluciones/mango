69abd3af38dc8a18f814d3313e5c894519ab2d90
Guillermo Gonzalez-Santander <guillermo.gonzalez@baobabsoluciones.es>
2023-12-21 15:56:26 +0100

    Testing build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,12 @@ release = "0.2.0"
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ["templates"]
+exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "furo"
+html_static_path = ["static"]
