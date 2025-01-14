a0fe9687a1a4c78d6801da4a1e1bca16733da179
Guillermo Valle <guillermo.valle@baobabsoluciones.es>
2025-01-14 13:34:20 +0100

    Change conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../mango/"))
+sys.path.insert(0, os.path.abspath("../../mango_time_series/"))
 
 
 project = "mango"
