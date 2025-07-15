8b778466f300d418c5660a29d81860976c2fb978
JuanCobo <juan.cobo@baobabsoluciones.es>
2025-07-15 14:16:03 +0200

    Update PyPI workflow to specify package name and add versioning to __init__.py

--- mango_calendar/mango_calendar/__init__.py
+++ mango_calendar/mango_calendar/__init__.py
@@ -1,1 +1,10 @@
 """Mango Calendar package."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("mango-calendar")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["__version__"]
