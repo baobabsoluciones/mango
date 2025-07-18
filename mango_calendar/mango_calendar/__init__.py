7810e8c9a1152a555eb191a829be795530e7a918
JuanCobo <juan.cobo@baobabsoluciones.es>
2025-07-18 11:30:17 +0200

    Add initial __init__.py for Mango Calendar package and define public API

--- mango_calendar/mango_calendar/__init__.py
+++ mango_calendar/mango_calendar/__init__.py
@@ -7,4 +7,7 @@ try:
 except PackageNotFoundError:
     __version__ = "unknown"
 
-__all__ = ["__version__"]
+from .calendar_features import get_calendar
+from .date_utils import get_covid_lockdowns, get_holidays_df
+
+__all__ = ["__version__", "get_calendar", "get_holidays_df", "get_covid_lockdowns"]
