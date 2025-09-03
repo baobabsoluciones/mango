f27d79927632b8a78c8438f01d4bf8e1478b12f8
NataliaGorrin <natalia.gorrin@baobabsoluciones.es>
2025-09-03 18:26:08 +0200

    update requirements check

--- mango/mango/tests/shared_module/test_requirement_check.py
+++ mango/mango/tests/shared_module/test_requirement_check.py
@@ -1,13 +1,12 @@
 import os
+from importlib.metadata import PackageNotFoundError
 from unittest import TestCase, mock
 
 from mango.shared.requirement_check import check_dependencies
-from pkg_resources import DistributionNotFound
-
 
 
 def raise_exception(*args, **kwargs):
-    raise DistributionNotFound()
+    raise PackageNotFoundError("mocklib")
 
 
 class TestRequirementCheck(TestCase):
@@ -27,15 +26,15 @@ class TestRequirementCheck(TestCase):
     def tearDownClass(cls) -> None:
         os.remove("mockup.toml")
 
-    @mock.patch("mango.shared.requirement_check.pkg_resources")
-    def test_check_dependencies(self, mock_pkg_resources):
-        mock_pkg_resources.require.value = None
+    @mock.patch("mango.shared.requirement_check.version")
+    def test_check_dependencies(self, mock_version):
+        mock_version.return_value = "1.0.0"
         flag = check_dependencies("option_1", pyproject_path="mockup.toml")
         self.assertTrue(flag)
 
-    @mock.patch("mango.shared.requirement_check.pkg_resources", autospec=True)
-    def test_missing_dependency(self, mock_pkg_resources):
-        mock_pkg_resources.require.side_effect = raise_exception
+    @mock.patch("mango.shared.requirement_check.version")
+    def test_missing_dependency(self, mock_version):
+        mock_version.side_effect = raise_exception
         flag = check_dependencies("option_2", pyproject_path="mockup.toml")
         self.assertFalse(flag)
 
